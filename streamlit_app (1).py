d0d04161c2391839ea5053e52b953156b20b7eb5
Juan091178 <secgeneralametxxiv@gmail.com>
2025-06-06 17:13:39 -0300

    Delete streamlit_app (1).py

--- streamlit_app (1).py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-st.markdown("""
-<h1 style='text-align: center; color: #0046be;'>Simulador Comparativo Salarial</h1>
-<h3 style='text-align: center; color: #444;'>Abril vs Mayo 2025 - AMET Tierra del Fuego</h3>
-""" , unsafe_allow_html=True)
