6b8cd3aa352d528b3f4abc50d2ec8a9ab798b79e
Juan091178 <secgeneralametxxiv@gmail.com>
2025-06-12 15:35:31 -0300

    Update simulador_amet_jerarquico_ajustado.py

--- simulador_amet_jerarquico_ajustado.py
+++ simulador_amet_jerarquico_ajustado.py
@@ -112,7 +112,7 @@ def calcular_total(cargos, cantidades, vi, puntajes_dict, descuentos, antiguedad
     adicional_jerarquico_total = basico_jerarquico * 0.30
 
     codigo = cargo.split(" - ")[0].strip()
-        descripcion = cargo.lower()
+    descripcion = cargo.lower()
         if "hora" in descripcion and (
             (codigo.startswith("3") or codigo.startswith("4")) and
             codigo not in codigos_excluidos_bonificacion
