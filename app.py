4bda6a14cdfb5d2738a22a6f62b972484248767d
Juan091178 <secgeneralametxxiv@gmail.com>
2025-06-18 16:58:44 -0300

    Delete app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-# app.py - versión corregida
-import streamlit as st
-import pandas as pd
-from PIL import Image
-from simulador import calcular_simulacion
-def cargar_datos(): ...
-# incluye uso de banner/logo + tabla resaltada para NETO
