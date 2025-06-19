404bb0c4b1985435f01747a4b2d0dcfd0c397867
Juan091178 <secgeneralametxxiv@gmail.com>
2025-06-19 15:11:07 -0300

    Delete simulador.py

--- simulador.py
+++
@@ -1,6 +0,0 @@
-# simulador.py - versión corregida
-from salario import calcular_basico_y_complementos
-from bonos import calcular_unidades_bono_reglamentaria, calcular_monto_bonos
-from descuentos import calcular_descuentos_legales, calcular_descuentos_gremiales
-def calcular_simulacion(...): ...
-# mismo código que antes con neto final = remunerativo - descuentos + bonos
